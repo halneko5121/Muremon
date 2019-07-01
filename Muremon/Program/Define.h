47cfcb1d479d176028d67db23f64589c806f962c
halneko <otsuichi5121@gmail.com>
2019-07-01 20:31:54 +0900

    update 置き換え。

--- Muremon/Program/Define.h
+++ Muremon/Program/Define.h
@@ -1,21 +1,47 @@
-//---------------------------------------------
-//
-//      デファイン(ゲーム内)
-//      作成開始日:3月19日
-//			更新日:3月24日
-//			作成者:平野
-//
-//---------------------------------------------
-
 #pragma once
+/******************************************************************
+ *	@file	Define.h
+ *	@brief	各種定義
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
 
-#define SAFE_RELEASE(p) { if(p) { (p)->Release(); (p)=NULL; } }
-#define SAFE_DELETE_ARRAY(p)	{if(p){delete[](p);		(p) = NULL;} }
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+#include <tchar.h>
+#include "D_Game.h"
 
-//追加↓三上3/26
-#define SAFE_DELETE(p)			{if(p){delete(p);		(p) = NULL;} }
+#define APP_SAFE_RELEASE(p)				{if(p){(p)->Release();	(p) = nullptr;} }
+#define APP_SAFE_DELETE_ARRAY(p)		{if(p){delete[](p);		(p) = nullptr;} }
+#define APP_SAFE_DELETE(p)				{if(p){delete(p);		(p) = nullptr;} }
+
+#ifdef _DEBUG
+#define	APP_PRINT(format, ...)								\
+		{													\
+			char temp[MAX_PATH];							\
+			sprintf_s(temp, MAX_PATH, format, __VA_ARGS__);	\
+			OutputDebugString(temp);						\
+		}										
+#define APP_PRINT_NL(format, ...)		{ APP_PRINT(format, __VA_ARGS__); OutputDebugString("\n"); }
+#else
+#define APP_PRINT(...)					{ }
+#define APP_PRINT_NL(...)				{ }
+#endif
+
+#define APP_ASSERT(p)					{ assert(p); }
+#define APP_ASSERT_FALSE()				{ assert(false); }
+#define APP_ASSERT_MESSAGE(p, str)		{ if(!(p)) { APP_PRINT_NL(str); } assert(p); }
+#define APP_ASSERT_FALSE_MESSAGE(str)	{ APP_ASSERT_MESSAGE(false, str); }
+#define APP_POINTER_ASSERT(p)			{ if(p == nullptr){ APP_ASSERT_FALSE(); } }
+
+#define SAFE_RELEASE(p) 				{if(p){(p)->Release(); 	(p) = nullptr;} }
+#define SAFE_DELETE_ARRAY(p)			{if(p){delete[](p);		(p) = nullptr;} }
+#define SAFE_DELETE(p)					{if(p){delete(p);		(p) = nullptr;} }
 #define MAX_ALPHA			(255)
 #define MAX_RGB				(255)
+#define MAX_STRING			(256)
 #define GAMESIZE_HEGHT		(600)
 #define GAMESIZE_WIDE		(800)
 

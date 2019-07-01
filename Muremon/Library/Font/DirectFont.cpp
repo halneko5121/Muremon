47794f812bef7e67e00b23a65c19d69d9ad61b73
halneko <otsuichi5121@gmail.com>
2019-07-01 20:39:25 +0900

    update 置き換え。

--- Muremon/Library/Font/DirectFont.cpp
+++ Muremon/Library/Font/DirectFont.cpp
@@ -1,125 +1,226 @@
-//---------------------------------------------
-//
-//      文字の設定
-//      作成開始日:3月17日
-//			更新日:3月17日
-//			作成者:平野
-//
-//---------------------------------------------
+/******************************************************************
+ *	@file	DirectFont.cpp
+ *	@brief	文字の設定
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
 
-//include
-#include "DirectFont.h"
+#include "Library/Font/DirectFont.h"
+#include "Program/Define.h"
 
-//////////////////////////////////////////////////////////
-//
-//      説明　：初期化を行う(メンバ初期化)
-//      引数　：LPDIRECT3DDEVICE9   pDevice    デバイス
-//　　　戻り値：HRESULT S_OK:成功   E_FAIL:失敗
-//
-//////////////////////////////////////////////////////////
-HRESULT C_DFont::InitDFont(LPDIRECT3DDEVICE9 pDevice)
+namespace
 {
-    //デバイスセット
-    fontDevice = pDevice;
-    //フォントサイズ指定
-    fontSize = D3DXVECTOR2(DEFAULT_FONT_SIZE_X,DEFAULT_FONT_SIZE_Y);
-    //色指定
-    fontColor = 0xFFFFFFFF;
-    //初期化
-	ZeroMemory(&fontDesc,sizeof(D3DXFONT_DESC));
-	fontDesc.Width			= (UINT)fontSize.x;				//平均文字幅
-	fontDesc.Height			= (UINT)fontSize.y;				//高さ(文字の大きさ)
-	fontDesc.Weight			= FW_NORMAL;					//文字の太さ(1～1000まで指定可能)
-	fontDesc.MipLevels		= 1;							//ミップマップレベル
-	fontDesc.Italic			= false;						//斜体にするならtrueを設定
-	fontDesc.CharSet			= SHIFTJIS_CHARSET;			//キャラクタ・セット(シフトJISを設定)
-	fontDesc.OutputPrecision	= OUT_DEFAULT_PRECIS;		//出力精度
-	fontDesc.Quality			= DEFAULT_QUALITY;			//品質
-	fontDesc.PitchAndFamily	= DEFAULT_PITCH | FF_DONTCARE;	//ピッチ
-	lstrcpy(fontDesc.FaceName,TEXT(""));					    //フォント名
+	// 基本のフォントサイズ
+	const D3DXVECTOR2 cDefaultFontSize = { 13, 40 };
+}
 
-	if(FAILED(D3DXCreateFontIndirect(fontDevice,&fontDesc,&pFont)))
+C_DFont* C_DFont::mInstance = nullptr;
+
+/**
+ * @brief	インスタンスの取得
+ */
+C_DFont*
+C_DFont::GetInstance()
+{
+	return mInstance;
+}
+
+/**
+ * @brief	インスタンスの生成
+ */
+void
+C_DFont::Create()
+{
+	APP_ASSERT_MESSAGE(mInstance == nullptr, "既に生成済みです");
+	mInstance = new C_DFont();
+}
+
+/**
+ * @brief	インスタンスの破棄
+ */
+void
+C_DFont::Destroy()
+{
+	APP_SAFE_DELETE(mInstance);
+}
+
+/**
+ * @brief	初期化を行う(メンバ初期化)
+ * @param	device    デバイス
+ * @return	S_OK:成功   E_FAIL:失敗
+ */
+HRESULT
+C_DFont::InitDFont(LPDIRECT3DDEVICE9 device)
+{
+    // デバイスセット
+    mFontDevice = device;
+    // フォントサイズ指定
+	mFontSize = cDefaultFontSize;
+    // 色指定
+    mFontColor = 0xFFFFFFFF;
+    // 初期化
+	ZeroMemory(&mFontDesc,sizeof(D3DXFONT_DESC));
+	mFontDesc.Width				= (UINT)mFontSize.x;			// 平均文字幅
+	mFontDesc.Height			= (UINT)mFontSize.y;			// 高さ(文字の大きさ)
+	mFontDesc.Weight			= FW_NORMAL;					// 文字の太さ(1～1000まで指定可能)
+	mFontDesc.MipLevels			= 1;							// ミップマップレベル
+	mFontDesc.Italic			= false;						// 斜体にするならtrueを設定
+	mFontDesc.CharSet			= SHIFTJIS_CHARSET;				// キャラクタ・セット(シフトJISを設定)
+	mFontDesc.OutputPrecision	= OUT_DEFAULT_PRECIS;			// 出力精度
+	mFontDesc.Quality			= DEFAULT_QUALITY;				// 品質
+	mFontDesc.PitchAndFamily	= DEFAULT_PITCH | FF_DONTCARE;	// ピッチ
+	lstrcpy(mFontDesc.FaceName,TEXT("小塚ゴシックStd H Bold"));	// フォント名
+
+	if(FAILED(D3DXCreateFontIndirect(mFontDevice, &mFontDesc, &mFont)))
 	{
-		MessageBox(NULL , TEXT("DirectFontのインターフェース作成失敗"),NULL,MB_OK);
+		MessageBox(NULL, TEXT("DirectFontのインターフェース作成失敗"), NULL, MB_OK);
 		return E_FAIL;
 	}
 
 	return S_OK;
 }
  
-//////////////////////////////////////////////////////////
-//
-//      説明　：フォントの色変更
-//      引数　：D3DCOLOR    alpha   アルファ値
-//              D3DCOLOR    red     赤の値
-//              D3DCOLOR    green   緑の値
-//              D3DCOLOR    blue    青の値
-//　　　戻り値：なし
-//
-//////////////////////////////////////////////////////////
-void C_DFont::ChangeFontColor(D3DCOLOR alpha , D3DCOLOR red , D3DCOLOR green , D3DCOLOR blue)
+/**
+ * @brief	フォントの色変更
+ * @param	alpha   アルファ値
+ * @param	red		赤の値
+ * @param	green   緑の値
+ * @param	blue    青の値
+ */
+void
+C_DFont::ChangeFontColor(D3DCOLOR alpha , D3DCOLOR red , D3DCOLOR green , D3DCOLOR blue)
 {
-    fontColor = D3DCOLOR_ARGB(alpha , red , green , blue);
+    mFontColor = D3DCOLOR_ARGB(alpha , red , green , blue);
 }
  
-//////////////////////////////////////////////////////////
-//
-//      説明　：フォントサイズ変更
-//      引数　：D3DXVECTOR2 size    大きさ指定(幅・高さ)
-//　　　戻り値：なし
-//
-//////////////////////////////////////////////////////////
-void C_DFont::ChangeFontSize(D3DXVECTOR2 size)
+/**
+ * @brief	フォントサイズ変更
+ * @param	size	大きさ指定(幅・高さ)
+ */
+void
+C_DFont::ChangeFontSize(D3DXVECTOR2 size)
 {
-    //フォントサイズ指定
-    fontSize = D3DXVECTOR2(size.x,size.y);
-    //初期化
-	ZeroMemory(&fontDesc,sizeof(D3DXFONT_DESC));
-	fontDesc.Width			= (UINT)fontSize.x;				//平均文字幅
-	fontDesc.Height			= (UINT)fontSize.y;				//高さ(文字の大きさ)
-	fontDesc.Weight			= FW_NORMAL;					//文字の太さ(1～1000まで指定可能)
-	fontDesc.MipLevels		= 1;							//ミップマップレベル
-	fontDesc.Italic			= false;						//斜体にするならtrueを設定
-	fontDesc.CharSet			= SHIFTJIS_CHARSET;			//キャラクタ・セット(シフトJISを設定)
-	fontDesc.OutputPrecision	= OUT_DEFAULT_PRECIS;		//出力精度
-	fontDesc.Quality			= DEFAULT_QUALITY;			//品質
-	fontDesc.PitchAndFamily	= DEFAULT_PITCH | FF_DONTCARE;	//ピッチ
-	lstrcpy(fontDesc.FaceName,TEXT(""));						//フォント名
-
-	if(FAILED(D3DXCreateFontIndirect(fontDevice,&fontDesc,&pFont)))
+    // フォントサイズ指定
+	if (size == NULL)
 	{
-		MessageBox(NULL , TEXT("DirectFontの設定に失敗しました"),NULL,MB_OK);
+		mFontSize = cDefaultFontSize;
+	}
+	else
+	{
+		mFontSize = D3DXVECTOR2(size.x, size.y);
+	}
+
+	mFontDesc.Width	 = static_cast<UINT>(mFontSize.x);	// 平均文字幅
+	mFontDesc.Height = static_cast<UINT>(mFontSize.y);	// 高さ(文字の大きさ)
+
+	if(FAILED(D3DXCreateFontIndirect(mFontDevice, &mFontDesc, &mFont)))
+	{
+		MessageBox(NULL, TEXT("フォントサイズ変更に失敗しました"), NULL, MB_OK);
 		return;
 	}
 
 	return;
 }
-  
-//////////////////////////////////////////////////////////
-//
-//      説明　：文字の描画
-//      引数　：LPSTR   str     文字列
-//              long    posX    X座標
-//              long    posY    Y座標
-//　　　戻り値：なし
-//
-//////////////////////////////////////////////////////////
-void C_DFont::DrawFont(LPSTR str , long posX , long posY)
+
+/**
+ * @brief	フォント変更
+ * @param	fontname	フォント名
+ * @return  なし
+ */
+void
+C_DFont::ChangeFontName(LPCSTR fontname)
 {
-	//表示位置を設定
-	RECT l_rect = {posX , posY , CW_USEDEFAULT , CW_USEDEFAULT};
+//#ifdef	_DEBUG
+	lstrcpy(mFontDesc.FaceName,fontname);						
+//#else
+//	WCHAR  wszData[20];
+//	LPWSTR data;
+//
+//	//WIDE文字に変換
+//	MultiByteToWideChar(CP_ACP,0,fontname,-1,wszData,20);
+//	data = wszData;
+//
+//	lstrcpy(fontDesc.FaceName,data);						
+//#endif
 
-	//色の設定
-	D3DCOLOR l_color = fontColor;
-
-	if(pFont)
+	if(FAILED(D3DXCreateFontIndirect(mFontDevice,&mFontDesc,&mFont)))
 	{
-		pFont->DrawTextA(
-			NULL,					//描画に使うスプライトの指定(NULLを指定すると勝手に決めてくれる)
-			str,					//描画する文字列
-			-1,						//文字数(-1を指定すると勝手に計算してくれる)
-			&l_rect,				//描画する領域
-			DT_LEFT | DT_NOCLIP,	//描画方法
-			l_color);				//文字の色
+		MessageBox(NULL, TEXT("フォント変更に失敗しました"), NULL, MB_OK);
+	}
+}
+
+/**
+ * @brief	文字の太さの変更
+ * @param	font_weight(1～1000まで指定可能)
+ */
+void
+C_DFont::ChangeWeight(UINT font_weight)
+{
+	// 文字の太さ(1～1000まで指定可能)
+	APP_ASSERT_MESSAGE(font_weight <= 1000, "文字の太さは1000以下になるようにして下さい");
+	mFontDesc.Weight = font_weight;
+
+	if(FAILED(D3DXCreateFontIndirect(mFontDevice, &mFontDesc, &mFont)))
+	{
+		MessageBox(NULL, TEXT("フォントの太さ変更に失敗しました"), NULL, MB_OK);
+		return;
+	}
+}
+
+/**
+ * @brief	斜体・通常変更
+ * @param	is_italic	斜体ならtrue／しないならfalse
+ */
+void
+C_DFont::ChangeItalic(bool is_italic)
+{
+	mFontDesc.Italic = is_italic;
+
+	if(FAILED(D3DXCreateFontIndirect(mFontDevice, &mFontDesc, &mFont)))
+	{
+		MessageBox(NULL, TEXT("斜体設定に失敗しました"), NULL, MB_OK);
+		return;
+	}
+}
+
+/**
+ * @brief	文字の描画(uFormatの設定をしたい場合)
+ * @param	str		文字列
+ * @param	pos_x	X座標
+ * @param	pos_y   Y座標
+ * @param	option　DrawTextのuFormatオプション
+ */
+void
+C_DFont::DrawFont(LPSTR str , long pos_x, long pos_y)
+{
+	DrawFont(str, pos_x, pos_y, DT_LEFT);
+}
+
+/**
+ * @brief	文字の描画(uFormatの設定をしたい場合)
+ * @param	str		文字列
+ * @param	pos_x	X座標
+ * @param	pos_y	Y座標
+ * @param	option　DrawTextのuFormatオプション
+ */
+void
+C_DFont::DrawFont(LPSTR str , long pos_x, long pos_y, DWORD option)
+{
+    // 表示位置を設定
+	RECT l_rect = { pos_x, pos_y, CW_USEDEFAULT, CW_USEDEFAULT};
+
+	// 色の設定
+	D3DCOLOR l_color = mFontColor;
+
+	if(mFont)
+	{
+		mFont->DrawTextA(
+			NULL,							// 描画に使うスプライトの指定(NULLを指定すると勝手に決めてくれる)
+			str,							// 描画する文字列
+			-1,								// 文字数(-1を指定すると勝手に計算してくれる)
+			&l_rect,						// 描画する領域
+			option | DT_LEFT | DT_NOCLIP,	// 描画方法
+			l_color);						// 文字の色
 	}
 }
--- Muremon/Library/Font/DirectFont.h
+++ Muremon/Library/Font/DirectFont.h
@@ -1,71 +1,108 @@
-//---------------------------------------------
-//
-//      文字の設定
-//      作成開始日:3月17日
-//			更新日:3月17日
-//			作成者:平野
-//
-//---------------------------------------------
+#pragma once
+/******************************************************************
+ *	@file	DirectFont.h
+ *	@brief	文字の設定
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
 
-#ifndef _DIRECTFONT_H_
-#define _DIRECTFONT_H_
-
-//define
-#define DEFAULT_FONT_SIZE_X (16)    //基本のフォントサイズ
-#define DEFAULT_FONT_SIZE_Y (30)    //基本のフォントサイズ
-
-//include
 #include "Library/Graphics/DirectGraphics.h"
+
+// オプション設定
+enum DrawTextOption
+{
+	cDrawTextOption_Left,			// 文字列を左揃えに
+	cDrawTextOption_Right,			// 文字列を右揃えに
+	cDrawTextOption_Center,			// 文字列を中央揃えに	
+	cDrawTextOption_Bottom,			// 文字列を下揃えに
+	cDrawTextOption_Vcenter,		// 文字列をy軸の中央に
+	cDrawTextOption_Break,			// 改行してくれる
+	cDrawTextOption_Tab,			// タブ
+};
 
 class C_DFont
 {
-private:
-    LPDIRECT3DDEVICE9   fontDevice; //文字描画専用で使うデバイス
-    LPD3DXFONT          pFont;      //DirectFontのインターフェース
-    D3DXFONT_DESC		fontDesc;   //フォント
-    D3DCOLOR            fontColor;  //フォントの色
-    D3DXVECTOR2         fontSize;   //フォントサイズ
 public:
-    //////////////////////////////////////////////////////////
-    //
-    //      説明　：初期化を行う(メンバ初期化)
-    //      引数　：LPDIRECT3DDEVICE9   pDevice    デバイス
-    //　　　戻り値：HRESULT S_OK:成功   E_FAIL:失敗
-    //
-    //////////////////////////////////////////////////////////
-    HRESULT InitDFont(LPDIRECT3DDEVICE9 pDevice);
+	/**
+	 * @brief	インスタンスの取得
+	 */
+	static C_DFont*	GetInstance();
 
-    //////////////////////////////////////////////////////////
-    //
-    //      説明　：フォントの色変更
-    //      引数　：D3DCOLOR    alpha   アルファ値
-    //              D3DCOLOR    red     赤の値
-    //              D3DCOLOR    green   緑の値
-    //              D3DCOLOR    blue    青の値
-    //　　　戻り値：なし
-    //
-    //////////////////////////////////////////////////////////
-    void ChangeFontColor(D3DCOLOR alpha , D3DCOLOR red , D3DCOLOR green , D3DCOLOR blue);
+	/**
+	 * @brief	インスタンスの生成
+	 */
+	static void		Create();
 
-    //////////////////////////////////////////////////////////
-    //
-    //      説明　：フォントサイズ変更
-    //      引数　：D3DXVECTOR2 size    大きさ指定(幅・高さ)
-    //　　　戻り値：なし
-    //
-    //////////////////////////////////////////////////////////
-    void ChangeFontSize(D3DXVECTOR2 size);
+	/**
+	 * @brief	インスタンスの破棄
+	 */
+	static void		Destroy();
 
-    //////////////////////////////////////////////////////////
-    //
-    //      説明　：文字の描画
-    //      引数　：LPSTR   str     文字列
-    //              long    posX    X座標
-    //              long    posY    Y座標
-    //　　　戻り値：なし
-    //
-    //////////////////////////////////////////////////////////
-    void DrawFont(LPSTR str , long posX , long posY);
+	/**
+	 * @brief	初期化を行う(メンバ初期化)
+	 * @param	device    デバイス
+	 * @return	S_OK:成功   E_FAIL:失敗
+	 */
+	HRESULT InitDFont(LPDIRECT3DDEVICE9 device);
+
+	/**
+	 * @brief	フォントの色変更
+	 * @param	alpha   アルファ値
+	 * @param	red		赤の値
+	 * @param	green   緑の値
+	 * @param	blue    青の値
+	 */
+	void ChangeFontColor(D3DCOLOR alpha, D3DCOLOR red, D3DCOLOR green, D3DCOLOR blue);
+
+	/**
+	 * @brief	フォントサイズ変更
+	 * @param	size	大きさ指定(幅・高さ)
+	 */
+	void ChangeFontSize(D3DXVECTOR2 size);
+
+	/**
+	 * @brief	フォント変更
+	 * @param	fontname	フォント名
+	 */
+	void ChangeFontName(LPCSTR fontname);
+
+	/**
+	 * @brief	文字の太さの変更
+	 * @param	font_weight(1～1000まで指定可能)
+	 */
+	void ChangeWeight(UINT font_weight);
+
+	/**
+	 * @brief	斜体・通常変更
+	 * @param	is_italic	斜体ならtrue／しないならfalse
+	 */
+	void ChangeItalic(bool is_italic);
+
+	/**
+	 * @brief	文字の描画
+	 * @param	str		文字列
+	 * @param	pos_x	X座標
+	 * @param	pos_y	Y座標
+	 */
+	void DrawFont(LPSTR str, long pos_x, long pos_y);
+
+	/**
+	 * @brief	文字の描画(uFormatの設定をしたい場合)
+	 * @param	str		文字列
+	 * @param	pos_x	X座標
+	 * @param	pos_y	Y座標
+	 * @param	option　DrawTextのuFormatオプション
+	 */
+	void DrawFont(LPSTR str, long pos_x, long pos_y, DWORD option);
+
+private:
+	static C_DFont*		mInstance;		// インスタンス
+    LPDIRECT3DDEVICE9   mFontDevice;	// 文字描画専用で使うデバイス
+    LPD3DXFONT          mFont;			// DirectFontのインターフェース
+    D3DXFONT_DESC		mFontDesc;		// フォント設定
+    D3DCOLOR            mFontColor;		// フォントの色
+    D3DXVECTOR2			mFontSize;		// フォントサイズ
 };
 
-#endif //_DIRECTFONT_H_
+static C_DFont* GetDirectFont() { return C_DFont::GetInstance(); }

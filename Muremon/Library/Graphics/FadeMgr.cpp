2d116a8b4a1e08e9fd8826ba52bc690cbe7737a9
halneko <otsuichi5121@gmail.com>
2019-07-05 02:15:34 +0900

    update フェード処理を実装しました。

--- Muremon/Library/Graphics/FadeMgr.cpp
+++ Muremon/Library/Graphics/FadeMgr.cpp
@@ -7,11 +7,18 @@
  ******************************************************************/
 
 #include "FadeMgr.h"
+#include "Vertex.h"
+
+FadeMgr* FadeMgr::mInstance = nullptr;
 
  /**
   * @brief	コンストラクタ
   */
-FadeMgr::FadeMgr()
+FadeMgr::FadeMgr(Vertex* vertex)
+	: mVertex(vertex)
+	, mAlpha(0)
+	, mFadeSpeed(0)
+	, mState(cFadeState_None)
 {
 }
 
@@ -20,4 +27,95 @@ FadeMgr::FadeMgr()
  */
 FadeMgr::~FadeMgr()
 {
+}
+
+/**
+ * @brief	インスタンスの取得
+ */
+FadeMgr*
+FadeMgr::GetInstance()
+{
+	return mInstance;
+}
+
+/**
+ * @brief	インスタンスの生成
+ */
+void
+FadeMgr::Create(Vertex* vertex)
+{
+	APP_ASSERT_MESSAGE(mInstance == nullptr, "既に生成済みです");
+	mInstance = new FadeMgr(vertex);
+}
+
+/**
+ * @brief	インスタンスの破棄
+ */
+void
+FadeMgr::Destroy()
+{
+	APP_SAFE_DELETE(mInstance);
+}
+
+/**
+ * @brief	フェードイン
+ */
+void
+FadeMgr::Update()
+{
+	switch (mState)
+	{
+	case cFadeState_FadeIn:
+		mAlpha += mFadeSpeed;
+		if (mAlpha <= 0)	mAlpha = 0;
+		break;
+
+	case cFadeState_FadeOut:
+		mAlpha += mFadeSpeed;
+		if (255 <= mAlpha)	mAlpha = 255;
+		break;
+
+	default:
+		break;
+	}
+
+	mVertex->SetColor(mAlpha, 255, 255, 255);
+}
+
+/**
+ * @brief	フェードイン
+ */
+void
+FadeMgr::FadeIn()
+{
+	mState = cFadeState_FadeIn;
+	mFadeSpeed = -5;
+}
+
+/**
+ * @brief	フェードアウト
+ */
+void
+FadeMgr::FadeOut()
+{
+	mState = cFadeState_FadeOut;
+	mFadeSpeed = 5;
+}
+
+/**
+ * @brief	フェードイン中か
+ */
+bool
+FadeMgr::IsFadeIn()
+{
+	return (mState == cFadeState_FadeIn);
+}
+
+/**
+ * @brief	フェードアウト中か
+ */
+bool
+FadeMgr::IsFadeOut()
+{
+	return (mState == cFadeState_FadeOut);
 }
--- Muremon/Library/Graphics/FadeMgr.h
+++ Muremon/Library/Graphics/FadeMgr.h
@@ -7,18 +7,76 @@
  *	管理者：三上
  ******************************************************************/
 
+class Vertex;
+
 class FadeMgr
 {
 public:
 	/**
 	 * @brief	コンストラクタ
 	 */
-	FadeMgr();
+	FadeMgr(Vertex* vertex);
 
 	/**
 	 * @brief	デストラクタ
 	 */
 	~FadeMgr();
 
+	/**
+	 * @brief	インスタンスの取得
+	 */
+	static FadeMgr*		GetInstance();
+
+	/**
+	 * @brief	インスタンスの生成
+	 */
+	static void			Create(Vertex* vertex);
+
+	/**
+	 * @brief	インスタンスの破棄
+	 */
+	static void			Destroy();
+
+	/**
+	 * @brief	更新
+	 */
+	void				Update();
+
+	/**
+	 * @brief	フェードイン
+	 */
+	void				FadeIn();
+
+	/**
+	 * @brief	フェードアウト
+	 */
+	void				FadeOut();
+
+	/**
+	 * @brief	フェードイン中か
+	 */
+	bool				IsFadeIn();
+
+	/**
+	 * @brief	フェードアウト中か
+	 */
+	bool				IsFadeOut();
+
+
 private:
+	enum FadeState
+	{
+		cFadeState_None,
+		cFadeState_FadeIn,
+		cFadeState_FadeOut,
+	};
+
+private:
+	static FadeMgr*		mInstance;		// インスタンス
+	Vertex*				mVertex;		// バーテックス
+	int					mAlpha;			// アルファ
+	int					mFadeSpeed;		// フェード速度
+	FadeState			mState;			// ステート
 };
+
+static FadeMgr* GetFadeMgr() { return FadeMgr::GetInstance(); }

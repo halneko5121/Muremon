906d49a37bfae7b8dccd5effc40e9299edc7eb46
halneko <otsuichi5121@gmail.com>
2019-07-06 16:06:45 +0900

    update 関数定義はヘッダーに。

--- Muremon/Library/StateMachine.h
+++ Muremon/Library/StateMachine.h
@@ -14,19 +14,43 @@ public:
 	/**
 	 * @brief	コンストラクタ
 	*/
-	StateMachine();
+	StateMachine()
+		: mMaxStateNum(0)
+		, mFirstStateIndex(-1)
+		, mCurrentStateIndex(-1)
+		, mPrevStateIndex(-1)
+		, mCurrentStateCounter(0)
+		, mPrevStateCounter(0)
+		, mIsChangeStateInExe(false)
+	{
+	}
 
 	/**
 	 * @brief	デストラクタ
 	 */
-	~StateMachine();
+	~StateMachine()
+	{
+	}
 
 	/**
 	 * @brief	初期化
 	 * @param	max_state_count		最大ステート数
 	 * @param	first_state_index	初期ステート番号
 	 */
-	void			initialize(int max_state_num, int first_state_index);
+	void			
+	initialize(int max_state_num, int first_state_index)
+	{
+		if ((cMaxRegistState <= max_state_num))
+		{
+			OutputDebugString("最大登録数を超えました");
+			assert(false);
+		}
+
+		mFirstStateIndex = first_state_index;
+		mMaxStateNum = max_state_num;
+		mCurrentStateIndex = mFirstStateIndex;
+		mPrevStateIndex = mFirstStateIndex;
+	}
 
 	/**
 	 * @brief	ステート関数の登録
@@ -35,70 +59,183 @@ public:
 	 * @param	ExeFunc		登録する実行関数
 	 * @param	ExitFunc	登録する終了関数
 	 */
-	void			registState(OWNER* owner, void (OWNER::*enterFunc)(), void (OWNER::*exeFunc)(), void (OWNER::*exitFunc)(int), int state_index);
+	void
+	registState(OWNER* owner, void (OWNER::*enterFunc)(), void (OWNER::*exeFunc)(), void (OWNER::*exitFunc)(int), int state_index)
+	{
+		// 範囲外
+		if ((state_index < 0) ||
+			(mMaxStateNum <= state_index))
+		{
+			OutputDebugString("指定ステート番号は範囲外です");
+			assert(false);
+		}
+
+		typedef Delegate<OWNER> DelegateImple;
+		mDelegate[state_index] = new DelegateImple(owner, enterFunc, exeFunc, exitFunc);
+	}
+
+	/**
+	 * @brief	ステートのリセット
+	 */
+	void
+	resetState()
+	{
+		mCurrentStateIndex = -1;
+		mCurrentStateCounter = 0;
+		mPrevStateIndex = -1;
+		mPrevStateCounter = 0;
+	}
 
 	/**
 	 * @brief	ステート関数の実行
 	 */
-	void			executeState();
+	void
+	executeState()
+	{
+		if ((mCurrentStateIndex == -1))
+		{
+			OutputDebugString("Initialize() を呼んで下さい");
+			assert(false);
+		}
+
+		mIsChangeStateInExe = false;
+
+		exeState_();
+
+		// ステート実行中にステート変更されていなかったらフレーム加算
+		if (!mIsChangeStateInExe)
+		{
+			mCurrentStateCounter++;
+		}
+	}
+
+	/**
+	 * @brief	ステートの終了
+	 */
+	void
+	exitState()
+	{
+		exitState_(-1);
+		changeState_(-1);
+	}
+
 	
 	/**
 	 * @brief	最初のステートに変更
 	 */
-	void			changeFirstState();
+	void
+	changeFirstState()
+	{
+		changeState_(mFirstStateIndex);
+		enterState_();
+	}
 
 	/**
 	 * @brief	指定ステートに変更
 	 * @param	state_index	ステート番号
 	 */
-	void			changeState(int next_state_index);
+	void
+	changeState(int next_state_index)
+	{
+		if ((next_state_index == -1))
+		{
+			OutputDebugString("Initialize() を呼んで下さい");
+			assert(false);
+		}
+
+		exitState_(next_state_index);
+		changeState_(next_state_index);
+		enterState_();
+	}
 
 	/**
 	 * @brief	指定ステートに変更
 	 * @param	next_state_index	ステート番号
 	 * @note	同じなら何もしない
 	 */
-	bool			changeStateIfDiff(int next_state_index);
+	bool
+	changeStateIfDiff(int next_state_index)
+	{
+		if (mCurrentStateIndex == next_state_index)
+		{
+			return false;
+		}
+
+		changeState(next_state_index);
+		return true;
+	}
 
 	/**
 	 * @brief	ステート番号の取得
 	 */
-	int				getStateIndex() const;
+	int
+	getStateIndex() const
+	{
+		return mCurrentStateIndex;
+	}
 
 	/**
 	 * @brief	前回のステート番号の取得
 	 */
-	int				getPrevStateIndex() const;
+	int
+	getPrevStateIndex() const
+	{
+		return mPrevStateIndex;
+	}
 
 	/**
 	 * @brief	登録ステート最大数の取得
 	 */
-	int				getMaxStateNum() const;
+	int
+	getMaxStateNum() const
+	{
+		return mMaxStateNum;
+	}
 
 	/**
 	 * @brief	ステート実行フレーム数の取得
 	 */
-	int				getStateCount();
+	int
+	getStateCount()
+	{
+		return mCurrentStateCounter;
+	}
 
 	/**
 	 * @brief	ステートが切り替わってから最初のフレームか
 	 */
-	bool			isStateCountFirst();
+	bool
+	isStateCountFirst()
+	{
+		return (getStateCount() == 0);
+	}
 
 	/**
 	 * @brief	指定したステートと同じステートか
 	 */
-	bool			isEqual(int state_index) const;
+	bool
+	isEqual(int state_index) const
+	{
+		return (state_index == getStateIndex());
+	}
 
 	/**
 	 * @brief	無効か？
 	 */
-	bool			isInvalid() const;
+	bool
+	isInvalid() const
+	{
+		return (getStateIndex() == -1);
+	}
 
 	/**
 	 * @brief	有効か？
 	 */
-	bool			isValid() const;
+	bool
+	isValid() const
+	{
+		return (!isInvalid());
+	}
 
 private:
 	template<class OWNER>
@@ -129,9 +266,9 @@ private:
 			if (mExeFunc != nullptr) (mOwner->*mExeFunc)();
 		}
 		virtual void
-		Delegate::exit()
+		Delegate::exit(int next_state_index)
 		{
-			if (mExitFunc != nullptr) (mOwner->*mExitFunc)();
+			if (mExitFunc != nullptr) (mOwner->*mExitFunc)(next_state_index);
 		}
 
 	private:
@@ -148,15 +285,15 @@ private:
 	void				changeState_(int state_index);
 	void				enterState_();
 	void				exeState_();
-	void				exitState_();
+	void				exitState_(int next_state_index);
 
 private:
-	int					mMaxStateNum;
-	int					mFirstStateIndex;
-	int					mCurrentStateIndex;
-	int					mPrevStateIndex;
-	int					mCurrentStateCounter;
-	int					mPrevStateCounter;
-	bool				mIsChangeStateInExe;
-	Delegate*			mDelegate[cMaxRegistState];
+	int					mMaxStateNum;					// 最大ステート数
+	int					mFirstStateIndex;				// 初期のステートインデックス
+	int					mCurrentStateIndex;				// 現在のステートインデックス
+	int					mPrevStateIndex;				// 前回のステートインデックス
+	int					mCurrentStateCounter;			// 現在のステート実行フレーム数 
+	int					mPrevStateCounter;				// 前回のステート実行フレーム数
+	bool				mIsChangeStateInExe;			// execute() 内部で changeState() を呼び出したか
+	Delegate<OWNER>*	mDelegate[cMaxRegistState];		// デリゲート配列
 };

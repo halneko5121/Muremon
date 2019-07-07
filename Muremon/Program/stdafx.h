0c2c9eacda8906ae22628715f40c15bff1ddfe34
halneko <otsuichi5121@gmail.com>
2019-07-07 13:17:00 +0900

    add 追加シワスレをコミット。

---
+++ Muremon/Program/stdafx.h
@@ -0,0 +1,40 @@
+#pragma once
+/******************************************************************
+ *	@file	stdafx.h
+ *	@brief	プリコンパイル済みヘッダー
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+#include <tchar.h>
+
+
+#define APP_SAFE_RELEASE(p)				{if(p){(p)->Release();	(p) = nullptr;} }
+#define APP_SAFE_DELETE_ARRAY(p)		{if(p){delete[](p);		(p) = nullptr;} }
+#define APP_SAFE_DELETE(p)				{if(p){delete(p);		(p) = nullptr;} }
+
+#ifdef _DEBUG
+#define	APP_PRINT(format, ...)							\
+		{												\
+			char temp[256];								\
+			sprintf_s(temp, 256, format, __VA_ARGS__);	\
+			OutputDebugString(temp);					\
+		}										
+#define APP_PRINT_NL(format, ...)		{ APP_PRINT(format, __VA_ARGS__); OutputDebugString("\n"); }
+#else
+#define APP_PRINT(...)					{ }
+#define APP_PRINT_NL(...)				{ }
+#endif
+
+#define APP_ASSERT(p)					{ assert(p); }
+#define APP_ASSERT_FALSE()				{ assert(false); }
+#define APP_ASSERT_MESSAGE(p, str)		{ if(!(p)) { APP_PRINT_NL(str); } assert(p); }
+#define APP_ASSERT_FALSE_MESSAGE(str)	{ APP_ASSERT_MESSAGE(false, str); }
+#define APP_POINTER_ASSERT(p)			{ if(p == nullptr){ APP_ASSERT_FALSE(); } }
+
+#define SAFE_RELEASE(p) 				{if(p){(p)->Release(); 	(p) = nullptr;} }
+#define SAFE_DELETE_ARRAY(p)			{if(p){delete[](p);		(p) = nullptr;} }
+#define SAFE_DELETE(p)					{if(p){delete(p);		(p) = nullptr;} }

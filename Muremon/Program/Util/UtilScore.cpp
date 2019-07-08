02cf235c291ea1150a90de8a093359dd1e11464a
halneko <otsuichi5121@gmail.com>
2019-07-08 15:31:59 +0900

    add UtilScore を追加しました。

---
+++ Muremon/Program/Util/UtilScore.cpp
@@ -0,0 +1,46 @@
+/******************************************************************
+ *	@file	UtilScore.cpp
+ *	@brief	スコア関連のUtil
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
+
+#include "UtilScore.h"
+#include "Program/System/GameScore.h"
+
+/**
+ * @brief	スコアの設定
+ */
+void
+UtilScore::setScore(int score)
+{
+	GetGameScore()->setScore(score);
+}
+
+/**
+ * @brief	スコアの取得
+ */
+int
+UtilScore::getScore()
+{
+	return GetGameScore()->getScore();
+}
+
+/**
+ * @brief	スコアの加算
+ */
+void
+UtilScore::addScore(int add_score)
+{
+	GetGameScore()->addScore(add_score);
+}
+
+/**
+ * @brief	スコアの減算
+ */
+void
+UtilScore::mulScore(int mul_score)
+{
+	GetGameScore()->mulScore(mul_score);
+}
---
+++ Muremon/Program/Util/UtilScore.h
@@ -0,0 +1,16 @@
+#pragma once
+/******************************************************************
+ *	@file	UtilScore.h
+ *	@brief	スコア関連のUtil
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
+
+namespace UtilScore
+{
+	void				setScore(int score);
+	int					getScore();
+	void				addScore(int add_score);
+	void				mulScore(int mul_score);
+}

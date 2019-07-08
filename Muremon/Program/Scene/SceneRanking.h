4178b550b74d3fb9087c63d48f888b23c10fb7b9
halneko <otsuichi5121@gmail.com>
2019-07-08 14:22:23 +0900

    clean ヘッダーになくてよいものeを cpp に移動しました。

--- Muremon/Program/Scene/SceneRanking.h
+++ Muremon/Program/Scene/SceneRanking.h
@@ -1,95 +1,13 @@
-//---------------------------------------------
-//
-//      ランキング
-//      作成開始日:	3月17日
-//			更新日:	3月18日
-//			作成者:	平野
-//
-//---------------------------------------------
 #pragma once
+/******************************************************************
+ *	@file	SceneRanking.h
+ *	@brief	ランキングシーン
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
 
 #include "Program/Scene/SceneBase.h"
-#include "Library/Graphics/Texture.h"
-#include "Library/Graphics/Vertex.h"
-
-#define POSITION_Y	(150)			// 名前のYの中心位置
-#define PLACE_POSITION_X	(50)	// 順位のフォントXの中心位置
-#define DOT_X	(100)
-#define NAME_POSITION_X	(150)		// 名前のXの中心位置
-#define SCORE_POSITION_X	(350)	// スコアのXの中心位置
-#define DISLOCATE_X	(50)			// Xの位置をずらす(但し、同じ値でずらさないと意味がない)
-#define DISLOCATE_Y	(100)			// Yの位置をずらす(但し、同じ値でずらさないと意味がない)
-
-#define FONT_ALPHA_DOWN	(10)
-
-#define RANKING_DRAWTIME	(120)
-
-#define Z_KEY_POSITION_X	(650)
-#define Z_KEY_POSITION_Y	(550)
-
-#define Z_KEY_ALPHA	(2)
-
-struct RANK
-{
-	char name[3];
-	int score;
-};
-
-enum RANK_FADE_MODE
-{
-	RANK_FADE_IN,
-	RANK_USUALLY,
-	RANK_FADE_OUT,
-};
-
-enum TEXTURE_DATA_RANKING
-{
-	T_RANKING_BG,
-	T_RANKING_FONT,
-};
-
-enum RECT_DATA_RANKING
-{
-	R_RANKING_BG,
-	R_FONT_A,
-	R_FONT_B,
-	R_FONT_C,
-	R_FONT_D,
-	R_FONT_E,
-	R_FONT_F,
-	R_FONT_G,
-	R_FONT_H,
-	R_FONT_I,
-	R_FONT_J,
-	R_FONT_K,
-	R_FONT_L,
-	R_FONT_M,
-	R_FONT_N,
-	R_FONT_O,
-	R_FONT_P,
-	R_FONT_Q,
-	R_FONT_R,
-	R_FONT_S,
-	R_FONT_T,
-	R_FONT_U,
-	R_FONT_V,
-	R_FONT_W,
-	R_FONT_X,
-	R_FONT_Y,
-	R_FONT_Z,
-	R_FONT_0,
-	R_FONT_1,
-	R_FONT_2,
-	R_FONT_3,
-	R_FONT_4,
-	R_FONT_5,
-	R_FONT_6,
-	R_FONT_7,
-	R_FONT_8,
-	R_FONT_9,
-	R_FONT_DOT,
-	//R_Z_KEY,
-};
 
 class SceneRanking : public SceneBase
 {

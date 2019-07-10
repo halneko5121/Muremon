4f256f81ce8feee5b0ae37432ce902b928a99349
halneko <otsuichi5121@gmail.com>
2019-07-10 17:13:49 +0900

    clean 不要コードを削除しました。

--- Muremon/Program/Util/UtilGraphics.h
+++ Muremon/Program/Util/UtilGraphics.h
@@ -14,8 +14,4 @@ namespace UtilGraphics
 {
 	LPDIRECT3DDEVICE9	getGraphicsDevice();
 	void				setTexture(Vertex* vertex, const Texture& texture, int texture_id);
-
-	int					getScore();
-	void				addScore(int add_score);
-	void				mulScore(int mul_score);
 }

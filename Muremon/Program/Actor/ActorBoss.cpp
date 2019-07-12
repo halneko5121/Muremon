86aca871db442cf5f52e36ed868293db7e35c994
halneko <otsuichi5121@gmail.com>
2019-07-12 16:25:28 +0900

    clean 未使用定数を削除しました。

--- Muremon/Program/Actor/ActorBoss.cpp
+++ Muremon/Program/Actor/ActorBoss.cpp
@@ -15,16 +15,12 @@ enum BOSS_CONTROLTYPE
 // ボス関連
 #define BOSS_INITIAL_LIFE			(3000)	//ボスの初期ライフ
 #define BOSS_GROW_LIFE				(1000)	//ボスが死んだ時増加していくライフ
-#define BOSS_ALPHA_APPEAR			(20)	//ボスが出現した時のアルファ増値
 #define BOSS_ALPHA_FALL				(5)		//ボスが滅した時のアルファ減値
 #define BOSS_APPEARANCE_POSITION	(950)	//ボスの出現中心位置
 #define BOSS_REFRESH_X_STOP			(550)	//スッキリモードのボスの止める中心座標
 #define BOSS_STABILITY_Y			(350)	//ボスY位置の固定
 #define BOSS_DAMAGE_COUNT			(50)	//ボスが攻撃を何回食らった時に痛がり表示
-#define BOSS_DAMAGE_TIME_DRAW		(60)	//ボスの痛がり表示時間
 #define BOSS_FALL_TIME				(60)
-#define BOSS_SPPEAR_TIME			(60)	//ボスがフェードアウトする時間
-#define BOSS_NO_DRAWTIME			(120)	//NOを描画する時間
 
 #define DAMAGE_RAND					(5)
 
@@ -34,11 +30,6 @@ enum BOSS_CONTROLTYPE
 #define NO_POSITION_Y				(150.f)
 
 #define BOSS_MOVECOUNT				(3)
-
-#define HIT_EFFECT_X				(100)
-#define HIT_FFFECT_Y				(450)
-#define MISSION_CLEAR_SCORE			(50000)	//ミッションクリア加算スコア
-#define BOSS_KO_SCORE				(10000)
 
 ActorBoss::ActorBoss(Texture* texture, Vertex* vertex)
 	: ActorBase(texture, vertex)

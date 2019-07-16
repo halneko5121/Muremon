18c80c2b207b94e63840acd2013bea03b6eb0f2a
halneko <otsuichi5121@gmail.com>
2019-07-16 16:43:33 +0900

    add ミッション基底を追加しました。

---
+++ Muremon/Program/Mission/MissionBase.cpp
@@ -0,0 +1,25 @@
+/******************************************************************
+ *	@file	MissionBase.cpp
+ *	@brief	ミッション基底
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
+
+#include "MissionBase.h"
+
+ /**
+  * @brief	コンストラクタ
+  */
+MissionBase::MissionBase(MissionId id)
+	: mId(id)
+{
+
+}
+
+/**
+ * @brief	デストラクタ
+ */
+MissionBase::~MissionBase()
+{
+}
---
+++ Muremon/Program/Mission/MissionBase.h
@@ -0,0 +1,24 @@
+#pragma once
+/******************************************************************
+ *	@file	MissionBase.h
+ *	@brief	ミッション基底
+ *
+ *	製作者：三上
+ *	管理者：三上
+ ******************************************************************/
+
+enum MissionId;
+
+class MissionBase
+{
+public:
+	MissionBase(MissionId id);
+	virtual ~MissionBase();
+
+	virtual void	update() = 0;
+	virtual void	draw() = 0;
+	virtual bool	isEnd() = 0;
+
+protected:
+	MissionId		mId;
+};

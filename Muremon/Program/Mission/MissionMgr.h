f1c83a425c8df4ddced0e32c95d5efe095d6c8f7
halneko <otsuichi5121@gmail.com>
2019-07-16 16:39:49 +0900

    add ミッションIDを追加しました。

--- Muremon/Program/Mission/MissionMgr.h
+++ Muremon/Program/Mission/MissionMgr.h
@@ -14,6 +14,26 @@
 
 class ActorBoss;
 
+enum MissionId
+{
+	cMissionId_Null = -1,
+	cMissionId_Mission1,		//『10秒以内に100回連打せよ！！』
+	cMissionId_Mission2,		//『10秒間でちょうど50回連打せよ！！』
+	cMissionId_Mission3,		//『10秒間でちょうど100回連打せよ！！』
+	cMissionId_Mission4,		//『「NIKUMANTOTUGEKI」と入力せよ！！』
+	cMissionId_Mission5,		//『「NIKUMANINSEKIRAKKAJUTU」と入力せよ！！』
+	cMissionId_Mission6,		//『「YOSITAROHIPATACK」と入力せよ！！』
+	cMissionId_Mission7,		//『「YOSITAROHUSENSHOOT」と入力せよ！！』
+	cMissionId_Mission8,		//『「NOPPOKOKEPPETI」と入力せよ！！』
+	cMissionId_Mission9,		//『「NOPPOBOKＵSIRIKOPUTA」と入力せよ！！』
+	cMissionId_Mission10,		//『10秒数えて前後1秒以内で「Ｚキー」を押せ！』
+	cMissionId_Mission11,		//『5秒数えて前後1秒以内で「Ｚキー」を押せ！』
+	cMissionId_Mission12,		//『10秒以内に「にくまん」の連打数を一番高くしろ！』
+	cMissionId_Mission13,		//『10秒以内に「よしたろう」の連打数を一番高くしろ！』
+	cMissionId_Mission14,		//『10秒以内に「のっぽ」の連打数を一番高くしろ！』
+	cMissionId_Count,
+};
+
 class MissionMgr
 {
 public:

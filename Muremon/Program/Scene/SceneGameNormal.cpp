30b806563dca7adca704fe0f27d7a98a2bd51d6f
halneko <otsuichi5121@gmail.com>
2019-07-19 15:22:53 +0900

    update ボスを先に生成するようにしました。

--- Muremon/Program/Scene/SceneGameNormal.cpp
+++ Muremon/Program/Scene/SceneGameNormal.cpp
@@ -95,6 +95,9 @@ SceneGameNormal::impleInit()
 	UtilGame::setGameModeNormal();
 	UtilGraphics::loadVertexAndTexture(mVertex, mTexture, "gamenormal");
 
+	// ボス
+	mBoss = dynamic_cast<ActorBoss*>(GetActorMgr()->createActor(cActorId_Boss, mTexture, mVertex));
+
 	// プレイヤー3種類分
 	for (int actor_id = cActorId_Noppo; actor_id <= cActorId_Yoshi; actor_id++)
 	{
@@ -104,8 +107,6 @@ SceneGameNormal::impleInit()
 			mActor[actor_id][j] = GetActorMgr()->createActor(static_cast<ActorId>(actor_id), mTexture, mVertex);
 		}
 	}
-	// ボス
-	mBoss = dynamic_cast<ActorBoss*>(GetActorMgr()->createActor(cActorId_Boss, mTexture, mVertex));
 
 	// 初期化
 	GetActorMgr()->init();

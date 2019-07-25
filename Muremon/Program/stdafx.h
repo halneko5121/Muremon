e5a3d7eb6b491401e54a1a581341e92785652cd0
halneko <otsuichi5121@gmail.com>
2019-07-25 11:15:56 +0900

    add 「Size」 を使えうようにしました。

--- Muremon/Program/stdafx.h
+++ Muremon/Program/stdafx.h
@@ -26,6 +26,7 @@
 #include <d3dx9math.h>
 #include "Library/Math/Vector.h"
 #include "Library/Math/Rect.h"
+#include "Library/Math/Size.h"
 
 #define new								::new(_NORMAL_BLOCK, __FILE__, __LINE__)
 #define PI								(D3DX_PI)
